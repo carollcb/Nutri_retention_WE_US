645e13c4b5915193ff6d006f9a05ddf2617bcfe1
Carolina Barbosa <carolina.cbarbosa@usp.com>
2023-04-19 17:10:15 -0600

    Updating scripts

--- scripts/03_pullingHydrolakesdata.R
+++ scripts/03_pullingHydrolakesdata.R
@@ -26,22 +26,75 @@ source("scripts/LAGOS_EDI.R")
 #Wu_Lagos_lakes_df <- as.data.frame(Wu_Lagos_lakes_reproj, xy=TRUE)
 #Wu_Lagos_lakes_hydro <- inner_join(hydrolakes_West_df, Wu_Lagos_lakes_df, by="lk_lt_d")
 
-Wu_Lagos_lakes_hydro <- st_read("shps/Wu_Lagos_lakes_hydro.shp")%>%
+wu_nla <- read.csv("data/NLAmodel_output_Western.csv")
+epa_nla <- read.csv("data/nla2012_wide_siteinfo_08232016.csv")
+lagos_info <- read.csv("data/lagos_dataset_info.csv")%>%
+  dplyr::select(lagoslakeid, nhdplusv2_comid, lake_nhdid, lake_lat_decdeg, lake_lon_decdeg, lake_centroidstate) %>%
+  rename(COMID2012 = nhdplusv2_comid) %>%
+  filter(lake_centroidstate %in% c("CA", "UT", "NV",
+                                   "WA", "OR", "ID",
+                                   "MT", "WY", "CO",
+                                   "NM", "AZ")) %>%
+  mutate(lagoslakeid=factor(lagoslakeid))
+
+epa_nla <- epa_nla %>%
+  rename(ID=SITE_ID) 
+
+wu_epa_nla <- inner_join(epa_nla, wu_nla, by = "ID")
+
+wu_epa_nla <- wu_epa_nla%>%
+  group_by(VISIT_NO=1)
+
+#filtering only interesting variables
+wu_epa_nla <- wu_epa_nla %>%
+  dplyr::select(ID, UID, AREA_HA, COMID2012, NARS_NAME, Area, Chla, TNmean, TPmean, N_Load, N_EN, N_DE, N_Outflow, P_Load, P_EN, P_DE, P_Outflow)
+
+wu_epa_nla_LAGOS_locus <- right_join(lagos_info, wu_epa_nla, by = "COMID2012") %>%
+  drop_na()
+
+#Wu + EPA + LAGOS-US Locus = 158 lakes
+#n_distinct(wu_epa_nla_LAGOS_locus$lagoslakeid) = 157 lakes
+
+#Wu + LAGOS-US Locus + Hydrolakes = 158 lakes
+Wu_Lagos_lakes_hydro_sp <- st_read("shps/Wu_Lagos_lakes_hydro.shp")%>% #all lakes
   rename(lagoslakeid = lagslkd)%>%
   mutate(EnrDepP_ratio = (P_EN/P_DE), EnrDepN_ratio = (N_EN/N_DE))%>%
   mutate(log_ResTim = log(Res_tim)) 
+
+mapview(Wu_Lagos_lakes_hydro_sp, zcol = "EnrDepP_ratio")
+
+#transforming in df and pulling only lakes with N and P retention (Pret >0) = 101 lakes!!
+Wu_Lagos_lakes_hydro_df <- Wu_Lagos_lakes_hydro_sp %>%
+  #as_tibble() %>% #no longer sf
+  as.data.frame() %>%
+  dplyr::select(lagoslakeid, lk_lt_d, lk_ln_d, P_DE, P_EN, N_DE, N_EN, Res_tim, EnrDepP_ratio, EnrDepN_ratio, log_ResTim)%>%
+  mutate(Pret = (P_DE - P_EN), Nret = (N_DE - N_EN)) %>% 
+  #filter(Pret >0, Nret>0) %>%
+  rename(lat = lk_lt_d, lon= lk_ln_d) %>%
+  drop_na() 
+
+library(ggmap)
+
+#140 lakes with N and P retention and residence time info
+p <- ggmap(get_stamenmap(bbox=c(-125, 25, -100, 50), zoom = 5, 
+                         #maptype='toner'))
+                         maptype ='terrain',
+                         color = 'color'))
+p + geom_point(aes(x = lon, y = lat,  col = log_ResTim, size = Pret), data = Wu_Lagos_lakes_hydro_df) +
+  theme(legend.position="left")
 
 # create scatterplot
 ggplot(Wu_Lagos_lakes_hydro, aes(x = EnrDepP_ratio, y = log_ResTim)) +
   geom_point()
 
-Wu_Lagos_lakes_hydro_few <- Wu_Lagos_lakes_hydro %>%
+Wu_Lagos_lakes_hydro_few <- Wu_Lagos_lakes_hydro_sp %>%
   #as_tibble() %>% #no longer sf
   as.data.frame() %>%
   dplyr::select(lk_lt_d, lk_ln_d, P_DE, P_EN, N_DE, N_EN, Res_tim, EnrDepP_ratio, EnrDepN_ratio, log_ResTim)%>%
  # mutate(Res_tim_yr = Res_tim/365) %>% #changing from days to years
   mutate(Pret = (P_DE - P_EN), Nret = (N_DE - N_EN)) %>% 
-  filter(Pret >0, Nret>0) %>%
+  mutate(NetPRet = log(P_EN - P_DE), NetNRet = log(N_EN - N_DE)) %>% 
+  #filter(Pret >0, Nret>0) %>%
   rename(lat = lk_lt_d, lon= lk_ln_d) %>%
   drop_na() 
 
@@ -50,29 +103,5 @@ M = cor(Wu_Lagos_lakes_hydro_few)
 corrplot(M, method = 'square', order = 'FPC', type = 'lower', diag = FALSE)
 corrplot.mixed(M, lower = 'shade', upper = 'pie', order = 'hclust')
 corrplot(M, order = 'hclust', addrect = 2)
-
-#Looking at Pload data
-candidate_sites_WE <- readRDS("data/TP_flow_gauges.rds") %>%
-  mutate(flow_station_id = as.character(flow_station_id)) %>%
-  dplyr::filter(is_lake_gage == "TRUE")%>%
-  dplyr::filter(station_id == flow_station_id)%>%
-  rename(sites = flow_station_id)
-
-mapview(Wu_Lagos_lakes_hydro) + mapview(candidate_sites_WE)
-  
-LAGOS_WE_sp <- st_as_sf(dt1,
-                        coords= c("lake_lon_decdeg", "lake_lat_decdeg"),
-                        crs=4326) %>%
-  filter(lake_centroidstate %in% c("CA", "UT", "NV",
-                                   "WA", "OR", "ID",
-                                   "MT", "WY", "CO",
-                                   "NM", "AZ")) %>%
-  mutate(lagoslakeid=factor(lagoslakeid))
-
-ggplot()+
-  geom_sf(data= LAGOS_WE_sp)+
-  geom_sf(data = candidate_sites_WE, color="red")
-
-mapview(LAGOS_WE_sp) + mapview(candidate_sites_WE, color = "yellow") #procurar code pra aumentar size do segundo
 
  
