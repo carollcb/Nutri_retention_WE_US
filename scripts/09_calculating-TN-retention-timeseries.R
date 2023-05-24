9868994aac2203da4de06c723bc8fcbb6d58511d
carollcb <cbarbosa@uwyo.edu>
2023-05-24 15:10:02 -0600

    Still in progress with TP retention calc (script 09)

--- scripts/09_calculating-TN-retention-timeseries.R
+++ scripts/09_calculating-TN-retention-timeseries.R
@@ -61,39 +61,32 @@ upstream_sites_lagos <- read.csv("data/candidate_sites_TP_TN_Lagos_lakes.csv",
                                  stringsAsFactors = FALSE
 )
 
-##Change to get the new rds files! (Alice comment: "make sure the loads consider daily discharge (interpolated P concentrations * daily discharge)
-phosphorus_conc_files <- list.files(path= "data/nwis_TP/",pattern = ".rds", full.names = T)%>%
+##Wait for the daily interpolated P conc data(Alice comment: "make sure the loads consider daily discharge (interpolated P concentrations * daily discharge)
+phosphorus_conc_files <- list.files(path= "data/new_Q_TP_TN/",pattern = ".rds", full.names = T)%>%
   map(readRDS) %>% 
   data.table::rbindlist(fill=TRUE)%>%
-  mutate(phosphorus_mgl = as.numeric(phosphorus_mgl))%>%
   rename(flow_station_id = site_no)
 
 phosphorus_conc_files$year <- year(phosphorus_conc_files$date_time)
 
-phosphorus_conc_yrs <- group_by(phosphorus_conc_files, 
-                                year, flow_station_id, flow_cfs) %>%
-  dplyr::summarize(annual_median_TP = mean(phosphorus_mgl))%>%
-  #rename(flow_station_id = site_no)
+phosphorus_conc_yrs <- phosphorus_conc_files %>%
+  na.omit() %>%
+  group_by(phosphorus_mgl, date_time, flow_station_id) %>%
+  dplyr::summarize(interpolated_TP = approxfun(phosphorus_mgl))%>%
   na.omit()%>%
   mutate(annual_median_TP_ugl = annual_median_TP*1000)%>%
   mutate(q_m3s = flow_cfs /35.315)
-  
-##Transform Q from ft3s-1 in m3s-1 and then Pin=(somatP loads/somator Q)
-
-upstream_conc_hydro <- inner_join(hydrolakes_upstream_sites, phosphorus_conc_yrs, by="flow_station_id")%>%
-  group_by(flow_station_id, res_time_yr)%>%
-  select(flow_station_id, year, res_time_yr, annual_median_TP_ugl, station_id, q_m3s)
-
-upstream_data <- upstream_conc_hydro%>%
-  group_by(station_id)
-  mutate(Pin = (sum(annual_median_TP_ugl)/(sum(q_m3s))))
 
 
-upstream_conc_hydro <- upstream_conc_hydro %>%
-  mutate(Pret_coef = ((1-(1.43/annual_median_TP_ugl))*((annual_median_TP_ugl)/(1 + (res_time_yr^0.5)))^0.88))%>% #nondimensional?
+P_retention <- phosphorus_conc_yrs%>%
+  group_by(station_id, year) %>%
+  mutate(Pin = (sum(phosphorus_mgl)/(sum(q_m3s)))) %>%
+  mutate(Pret_coef = ((1-(1.43/Pin))*((Pin)/(1 + (res_time_yr^0.5)))^0.88))%>% #nondimensional?
   #rename(flow_station_id = site_no)
   na.omit()
 
+
+#Merging with ts data - check it again later!
 Pretention_lakes <- left_join(upstream_sites_lagos, upstream_conc_hydro, by= "station_id")%>%
   rename(water_year = year)%>%
   mutate(Pret_coef=as.numeric(Pret_coef))%>%
